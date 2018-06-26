c3bda698e882c66d4c1e896c6ef51ee93f835bfe
Luiz Fernando Cotta Martins <luiz.martins@griffith.ie>
2018-06-26 17:33:00 +0100

    Added navbar

--- public/register.php
+++ public/register.php
@@ -94,7 +94,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up</title>
+    <title>Skills Matrix - Registration</title>
     <link href="style.scss" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
