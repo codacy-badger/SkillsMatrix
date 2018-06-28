7caf5d64ce59a4205d38ba860d89b56a11be4274
Luiz Martins <luiz1361@gmail.com>
2018-06-28 02:37:50 +0100

    Fixed init

--- build/init.sh
+++ build/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+mkdir mysql
+mkdir www
+
 docker-compose up --build -d
 
 git clone https://github.com/luiz1361/SkillsMatrix.git www/
 
-cp db.inc.php www/SkillsMatris/private/
+cp db.inc.php www/private/
