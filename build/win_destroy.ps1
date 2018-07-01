f12142c94e4a08113034ec572e335d344bc51466
Luiz Fernando Cotta Martins <luiz1361@gmail.com>
2018-07-02 00:18:48 +0100

    Changes to dump and destroy script

--- build/win_destroy.ps1
+++ build/win_destroy.ps1
@@ -4,4 +4,6 @@ docker rm docker_compose_docker_db_image_1
 docker rm docker_compose_docker_web_image_1
 docker rmi docker_compose_docker_db_image
 docker rmi docker_compose_docker_web_image
+docker rmi mysql
+docker rmi php
 rm -r -fo ./mysql
