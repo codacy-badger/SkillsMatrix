bc6b8b51381c116eaed3e95f7e0beef3262506d1
Luiz Fernando Cotta Martins <luiz1361@gmail.com>
2018-07-02 00:00:06 +0100

    Change to destroy

--- build/bash_destroy.sh
+++ build/bash_destroy.sh
@@ -3,4 +3,6 @@ docker stop docker_compose_docker_db_image_1
 docker stop docker_compose_docker_web_image_1
 docker rm docker_compose_docker_db_image_1
 docker rm docker_compose_docker_web_image_1
+docker rmi docker_compose_docker_db_image
+docker rmi docker_compose_docker_web_image
 rm -Rf ./mysql
