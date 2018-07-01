8dcd0bdd0adbf306a5cbcd0810134290e4012e98
Luiz Fernando Cotta Martins <luiz1361@gmail.com>
2018-07-02 00:21:44 +0100

    Changes to destroy scripts

--- build/bash_destroy.sh
+++ build/bash_destroy.sh
@@ -5,6 +5,6 @@ docker rm docker_compose_docker_db_image_1
 docker rm docker_compose_docker_web_image_1
 docker rmi docker_compose_docker_db_image
 docker rmi docker_compose_docker_web_image
-docker rmi mysql
-docker rmi php
+docker rmi mysql:5.7
+docker rmi php:7.0-apache
 rm -Rf ./mysql
