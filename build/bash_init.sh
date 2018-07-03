96d029e82c1ddd53ba4b41379b9f8c854ccc220c
Luiz Fernando Cotta Martins <luiz1361@gmail.com>
2018-07-03 01:13:50 +0100

    Changes to build

--- build/bash_init.sh
+++ build/bash_init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ./bash_destroy.sh
+mv ../www docker_compose/
 git pull
 cd docker_compose && docker-compose up --build --force-recreate -d
--- build/win_init.ps1
+++ build/win_init.ps1
@@ -1,4 +1,5 @@
 ./win_destroy.ps1
 git pull
+mv ../www docker_compose/
 cd docker_compose
 docker-compose up --build --force-recreate -d
